7a7a07a4d94d0081a1cce4b91faf1b376a5c424d
Asif Navalur <asifnavalur@users.noreply.github.com>
2018-04-10 14:52:32 +0530

    Update conf.js

--- UI5Example/conf.js
+++ UI5Example/conf.js
@@ -4,7 +4,10 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+        args: ["--headless", "--disable-gpu", "--window-size=800x600"]
+    }
   },
   params: {
     auth: {
